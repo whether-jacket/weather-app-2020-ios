b57bbff2e1bacf1c4313ca70ce122650b103b5ed
Sami Eljabali <seljabali@users.noreply.github.com>
2020-10-04 03:45:41 -0700

    Cities page (#23)
    * cities page
    
    * swipe & reorder
    
    * convert to view controller
    
    * add city button

--- template/template/ui/cities/CitiesViewController.swift
+++ template/template/ui/cities/CitiesViewController.swift
@@ -1,10 +1,16 @@
 import UIKit
 import SnapKit
 
-class CitiesViewController: BaseViewController {
-
-    private let button = UIButton()
-
+class CitiesViewController: BaseViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    private var cities = [
+        CityTableItem(id: 1, cityName: "San Francisco", regionName: "CA"),
+        CityTableItem(id: 1, cityName: "San Diego", regionName: "CA"),
+        CityTableItem(id: 1, cityName: "Tempe", regionName: "AZ")
+    ]
+    private let citiesTableView = UITableView()
+    private let addCityButton = UIButton(type: .contactAdd)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         initializeViews()
@@ -12,16 +18,82 @@ class CitiesViewController: BaseViewController {
     }
 
     private func initializeViews() {
-        button.titleColorForNormal = UIColor.black
-        button.setTitle("Hello Cities", for: .normal)
-        view.addSubview(button)
+        citiesTableView.apply {
+            $0.delegate = self
+            $0.dataSource = self
+            $0.isEditing = true
+            $0.register(
+                CityTableViewCell.self,
+                forCellReuseIdentifier: CityTableViewCell.IDENTIFIER
+            )
+            $0.separatorStyle = .none
+            $0.tableFooterView = UIView()
+            $0.rowHeight = Dimens.WidgetCellHeight.cgFloat
+            view.addSubview($0)
+        }
+        addCityButton.apply {
+            $0.setOnTapListener(target: self, action: #selector(onCityButtonTapped))
+            view.addSubview($0)
+        }
     }
 
     private func setConstraints() {
-        button.snp.makeConstraints { (make) -> Void in
-            make.top.equalTo(view.safeAreaLayoutGuide).offset(VerticalSpacings.m)
-            make.left.equalTo(view.safeAreaLayoutGuide).offset(HorizontalSpacings.m)
+        addCityButton.snp.makeConstraints { (make) -> Void in
             make.right.equalTo(view.safeAreaLayoutGuide).offset(-HorizontalSpacings.m)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-VerticalSpacings.m)
         }
+        citiesTableView.snp.makeConstraints { (make) -> Void in
+            make.top.left.right.equalTo(view.safeAreaLayoutGuide)
+            make.bottom.equalTo(addCityButton.snp.top)
+        }
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cities.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let currentCity = self.cities[indexPath.row]
+        var cell: CityTableViewCell? = tableView.dequeueReusableCell(withIdentifier: CityTableViewCell.IDENTIFIER, for: indexPath) as? CityTableViewCell
+        if cell == nil || cell?.detailTextLabel == nil {
+            cell = CityTableViewCell(style: .subtitle, reuseIdentifier: CityTableViewCell.IDENTIFIER)
+        }
+        cell!.apply {
+            $0.applyTheme(ThemeManager.instance.getCurrentTheme())
+            $0.update(with: currentCity)
+        }
+        return cell!
+    }
+    
+    @objc private func onCityButtonTapped() {
+        log.verbose("we tapped on add city button")
+    }
+}
+
+// MARK: Swiping
+extension CitiesViewController {
+     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        return UISwipeActionsConfiguration(actions: [
+            makeDeleteContextualAction(forRowAt: indexPath)
+        ])
+    }
+    
+     private func makeDeleteContextualAction(forRowAt indexPath: IndexPath) -> UIContextualAction {
+        return UIContextualAction(style: .destructive, title: Strings.Delete) { (action, swipeButtonView, completion) in
+            log.verbose("we are swipe to deleting: \(indexPath.row)")
+            self.cities.remove(at: indexPath.row)
+            self.citiesTableView.deleteRows(at: [indexPath], with: .fade)
+            completion(true)
+         }
+     }
+}
+
+// MARK: Reordering
+extension CitiesViewController {
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        log.verbose("we are moving \(sourceIndexPath.row) => \(destinationIndexPath.row)")
+        let movedObject = self.cities[sourceIndexPath.row]
+        self.cities.remove(at: sourceIndexPath.row)
+        self.cities.insert(movedObject, at: destinationIndexPath.row)
     }
 }
---
+++ template/template/ui/cities/CityTableItem.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+struct CityTableItem {
+    var id : Int
+    var cityName : String
+    var regionName : String
+}
